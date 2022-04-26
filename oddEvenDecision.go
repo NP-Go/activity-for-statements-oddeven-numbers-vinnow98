d921006f5c86939ea07a86e327e1f1875bf6137e
vinnow98 <evinwoon@gmail.com>
2022-04-26 19:11:28 +0800

    Completed

--- oddEvenDecision.go
+++ oddEvenDecision.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert code here
+	var userNo int
+	fmt.Print("Enter a number")
+	fmt.Scanln(&userNo)
+	if userNo%2 == 1 {
+		fmt.Println("This number is odd")
+	} else if userNo%2 == 0 {
+		fmt.Println("This number is even")
+	} else {
+		fmt.Println("Please enter a number")
+	}
 }
